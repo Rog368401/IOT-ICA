e25621f205f670ee759f8cc28a57e03989c72ee7
Rory O'Gorman <119594586+Rog368401@users.noreply.github.com>
2025-01-09 14:46:19 +0000

    This is the code that will be used for my tilt sensor

---
+++ tilt-sensor.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import time
+import RPi.GPIO as GPIO
+
+channel = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def loop():
+    previous_state = None  
+    while True:
+        current_state = GPIO.input(channel)  
+        if current_state == GPIO.LOW: 
+            if previous_state != "Tilt Detected": 
+                print("Tilt Detected")
+                previous_state = "Tilt Detected"
+        else:  # No tilt
+            if previous_state != "No Tilt":  
+                print("No Tilt")
+                previous_state = "No Tilt"
+        time.sleep(0.1)  
+
+if __name__ == '__main__':
+    try:
+        loop()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
